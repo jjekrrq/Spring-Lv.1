0902829b67d06dd4a55ea8b2b550f7a7d0eef185
Lee Seungchang <jjekrrq01@naver.com>
2024-07-02 10:37:11 +0900

    feat : repository test case complete

--- src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -2,10 +2,23 @@ package hello.hello_spring.repository;
 
 import hello.hello_spring.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+// 테스트는 서로 순서가 상관없이, 의존관계에 상관 없이 설계가 되어야 한다.
+// 그러기 위해서는 하나의 메소드가 끝날 때마다 메모리를 비워줘야 함.
+// TDD : 테스트를 먼저 만들고 그에 맞는 클래스, 메소드를 작성하는 것을 '테스트 주도 개발'이라 함.
+
 class MemoryMemberRepositoryTest {
-    MemberRepository memberRepository = new MemoryMemberRepository();
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    // @AfterEach : 테스트케이스 하나가 끝날때마다 아래 afterEach()메소드가 실행되는 콜백 메서드.
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();
+    }
 
     @Test
     public void save(){
@@ -41,5 +54,20 @@ class MemoryMemberRepositoryTest {
 //        Member result = memberRepository.findByName("spring2").get();
 
         Assertions.assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member = new Member();
+        member.setName("spring1");
+        memberRepository.save(member);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberRepository.save(member2);
+
+        List<Member> members = memberRepository.findAll();
+
+        Assertions.assertThat(members.size()).isEqualTo(2);
     }
 }
